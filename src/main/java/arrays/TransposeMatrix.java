87eaa317f17402f1457e02d2957414f2b6050835
adiltst <adil.testelin59@hotmail.fr>
2023-08-20 16:56:00 +0200

    feat(transposeMatrix): validate transposeMatrix algorithm

--- src/main/java/arrays/TransposeMatrix.java
+++ src/main/java/arrays/TransposeMatrix.java
@@ -4,14 +4,29 @@ import java.util.Arrays;
 
 public class TransposeMatrix {
 
+    /**
+     * Given a 2D integer array matrix, return the transpose of matrix.
+     * <p>
+     * The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     * Output: [[1,4,7],[2,5,8],[3,6,9]]
+     * Example 2:
+     * <p>
+     * Input: matrix = [[1,2,3],[4,5,6]]
+     * Output: [[1,4],[2,5],[3,6]]
+     */
+
     public static int[][] transposeMatrix(int[][] matrix) {
         int row = matrix.length;
         int columns = matrix[0].length;
 
-        int[][] result = new int[row][columns];
+        int[][] result = new int[columns][row];
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < row; j++) {
                 result[i][j] = matrix[j][i];
             }
         }
